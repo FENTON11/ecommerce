05a6ed000ac82bbc73ac09c37000e31d083b13dc
FENTON11 <fencurt99@gmail.com>
2025-12-05 17:29:46 +0300

    added schema

---
+++ src/lib/db/schema/index.ts
@@ -0,0 +1,23 @@
+export * from "@/lib/db/schema/user";
+export * from "@/lib/db/schema/session"
+export * from "@/lib/db/schema/account";
+export * from "@/lib/db/schema/verification";
+export * from "@/lib/db/schema/guest";
+
+export * from "@/lib/db/schema/addresses";
+export * from "@/lib/db/schema/brands";
+export * from "@/lib/db/schema/categories";
+export * from "@/lib/db/schema/collections";
+export * from "@/lib/db/schema/products";
+export * from "@/lib/db/schema/images";
+export * from "@/lib/db/schema/variants";
+export * from "@/lib/db/schema/reviews";
+export * from "@/lib/db/schema/carts";
+export * from "@/lib/db/schema/orders";
+export * from "@/lib/db/schema/payments";
+export * from "@/lib/db/schema/coupons";
+export * from "@/lib/db/schema/wishlists";
+
+export * from "@/lib/db/schema/filters/genders";
+export * from "@/lib/db/schema/filters/colors";
+export * from "@/lib/db/schema/filters/sizes";
